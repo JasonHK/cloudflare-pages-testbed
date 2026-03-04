b7230260a1e03ca41d38196caa1d38ef8af7edc3
Jason Kwok <git@jasonhk.dev>
2026-03-05 01:32:45 +0800

    Updated middleware

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,8 +1,8 @@
 export const onRequest: PagesFunction = async (request) =>
 {
     const response = await request.next();
+    response.headers.append("X-Middleware-Headers", "1");
     console.log("Middleware Headers:");
     console.log(JSON.stringify(Array.from(response.headers), null, 2));
-    response.headers.append("X-Middleware-Headers", "1");
     return response;
 };
